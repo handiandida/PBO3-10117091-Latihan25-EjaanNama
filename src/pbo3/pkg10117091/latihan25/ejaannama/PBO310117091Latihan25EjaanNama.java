c9acadfb5b57106fba5a264a6c4edb08938da13b
Dida Handian <handiandida@gmail.com>
2018-10-01 20:34:33 +0700

    EjaanNama

---
+++ src/pbo3/pkg10117091/latihan25/ejaannama/PBO310117091Latihan25EjaanNama.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117091.latihan25.ejaannama;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Lenovo
+ * Nama         : Dida Handian
+ * Kelas        : IF-3 
+ * Nim          : 10117091 
+ * Deskripsi    : Program ini dapat meng eja nama yang di inputkan oleh user
+ */
+public class PBO310117091Latihan25EjaanNama {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nama;
+        int panjang;
+        char huruf; 
+        Scanner scn = new Scanner(System.in);
+        
+        System.out.print("Masukkan nama depan Anda untuk dieja : ");
+        nama = scn.next();
+        panjang = nama.length();
+        
+        System.out.println("\nEjaan untuk "+" \""+nama+"\" adalah : ");
+
+        for (int i = 0; i < panjang; i++) {
+            System.out.println("Huruf ke-"+(i+1)+" : "+nama.charAt(i));
+        }
+    }
+    
+}
